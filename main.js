a30333805d6fccf825c798f34a8d76ff05823603
André de Oliveira <103377724+Andreprado20@users.noreply.github.com>
2022-12-21 19:32:04 -0300

    Update main.js
    Update that includes Template Literals and Javascript Promises

--- main.js
+++ main.js
@@ -63,6 +63,8 @@ const user = {
   nickname: 'Dedeco'
 };
 
+
+
 {/*
 //const { name, ...rest } = user
 const arrays = [1,2,3,3,5,6,7,8,9,10]
@@ -102,7 +104,7 @@ document.body.innerText = user.address[key]
 
 // Array Methods - map()
 
-const array = [1,2,3,4,5, "klo"]
+const array = [1,2,3,4,5, 6,7,8,9,10, "klo"]
 
 //option1 - using 'for'
 {/*
@@ -139,8 +141,134 @@ const novoArray = array
 document.body.innerText = JSON.stringify(novoArray)
 */}
 
+//every
+{/*
 const todosItensSaoNumeros = array.every(item => typeof item == 'number')
 
 document.body.innerText = JSON.stringify(todosItensSaoNumeros)
+*/}
 
+//some
+{/*
+const peloMenosUmItemNaoENumero = array.some( item => typeof item != 'number')
 
+document.body.innerText = JSON.stringify(peloMenosUmItemNaoENumero)
+
+*/}
+
+//find and findIndex
+{/*
+const par = array.find (item => item % 2 == 0);
+document.body.innerText = JSON.stringify(par)
+*/};
+
+{/*
+const parIndex = array.findIndex (item => item % 2 == 0);
+document.body.innerText = JSON.stringify(parIndex)
+*/};
+
+{/*
+const soma = array.reduce((acc, item) => {
+  //document.body.innerText += acc +  ", " + item + "---"
+  return acc + item;
+}, 0);
+
+document.body.innerText = JSON.stringify(soma)
+*/};
+
+{/*
+const mapFilter = array.map(item => {
+  if(item % 2 == 0){
+    return item * item;
+  }
+}).filter(item => item % 2 == 0);
+
+document.body.innerText = JSON.stringify(mapFilter)
+*/}
+
+// Template literals
+{/*
+let name = 'André';
+const message = `Bem Vindo, ${name ? name: 'visitante' }`;
+
+document.body.innerText = message;
+*/}
+
+// Promises
+
+// .then/ .catch
+
+{/*
+const soma2 = (a, b) => { 
+  return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(a + b);
+  }, 2000);
+});
+}
+
+soma2(1, 3)
+.then(soma => {
+  document.body.innerText = soma
+})
+.catch(err => {
+  console.log(err)
+})
+*/}
+
+// fetch
+
+{/*
+fetch('https://api.github.com/users/Andreprado20')
+.then(response => {
+  response.json().then(body => {
+    console.log(body);
+  })
+})
+.catch(err => {
+  console.log(err)
+})
+
+*/}
+
+//rewritting:
+
+{/*
+fetch('https://api.github.com/users/Andreprado20')
+.then(response => {
+  return response.json();
+})
+.then(body => {
+  console.log(body);
+})
+.catch(err => {
+  console.log(err)
+})
+.finally(() => {
+  console.log('foi')
+})
+*/}
+
+//async and await
+
+{/*
+async function buscaDados(){
+  try{
+    const response = await fetch('https://api.github.com/users/Andreprado20');
+
+    const body = await response.json();
+    console.log(body);
+    return body.name;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    console.log('foi');
+  }
+}
+
+//buscaDados();
+
+buscaDados().then( name => {
+  console.log(name)
+});
+*/}
